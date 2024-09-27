9241f87fc9a75a1659b77c7345dea2013f233d09
Y <ykgt@ns366421.ip-176-31-121.eu>
2024-09-27 15:55:45 +0200

    Create Left menu

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,50 @@
 <script setup lang="ts">
 // import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-import NavView from './components/nav/NavView.vue'
+import NavView from '@/components/nav/NavView.vue'
+import SidebarMenu from '@/components/nav/SidebarMenu.vue'
 </script>
 
 <template>
+  <SidebarMenu />
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <div style="border: 1px solid black">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24px"
+        viewBox="0 -960 960 960"
+        width="24px"
+        fill="#5f6368"
+        :style="{ fill: 'var(--vt-c-text-dark-3)' }"
+      >
+        <path
+          d="M383-480 200-664l56-56 240 240-240 240-56-56 183-184Zm264 0L464-664l56-56 240 240-240 240-56-56 183-184Z"
+        />
+      </svg>
+    </div>
+
+    <img src="@/assets/keyboard_double_arrow_right.svg" alt="" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
       <NavView />
     </div>
+    <svg class="shape-twitter-3">
+      <use xlink:href="@/assets/keyboard_double_arrow_right.svg#arrow" />
+    </svg>
   </header>
 
   <RouterView />
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.shape-twitter-3 {
+  fill: purple;
+  width: 24px;
+  height: 24px;
+}
+
 header {
   line-height: 1.5;
   max-height: 100vh;
---
+++ src/components/nav/SidebarMenu.vue
@@ -0,0 +1,110 @@
+<template>
+  <aside :class="['flex', 'transition-in-out', { expanded: toggle }]" ref="asideWidth">
+    <div class="top-wrapper transition-in-out flex flex-align-center flex-justify-between">
+      <div class="flex flex-fill flex-align-center flex-justify-center">
+        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="30" height="30" />
+        <div>Menu</div>
+      </div>
+
+      <div
+        @click="() => (toggle = !toggle)"
+        class="arrow-ctn flex flex-align-center flex-justify-center transition-in-out"
+      >
+        <svg class="arrow transition-in-out">
+          <use xlink:href="@/assets/keyboard_double_arrow_right.svg#arrow" />
+        </svg>
+      </div>
+    </div>
+
+    <ul>
+      <li v-for="n in 100" :key="'li' + n">Item {{ n + misc[n % 3] }}</li>
+    </ul>
+  </aside>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref, useTemplateRef } from 'vue'
+
+const toggle = ref<boolean>(false)
+const misc = [' Lorem ipsum dolor sit amer oulala et encore plus', ' Item ', ' Réalisations ']
+const topWrapper = useTemplateRef('asideWidth')
+
+onMounted(() => {
+  const sidebarWidth = topWrapper.value?.clientWidth
+  if (sidebarWidth) {
+    document.documentElement.style.setProperty('--sidebar-custom-width', sidebarWidth + 'px')
+  }
+})
+</script>
+
+<style scoped lang="scss">
+$margin: 3rem;
+
+aside {
+  color: var(--vt-c-white-soft);
+  position: fixed;
+  left: calc(var(--sidebar-custom-width) * -1);
+  z-index: var(--z-index-top);
+
+  .arrow-ctn {
+    width: $margin;
+    height: $margin;
+    cursor: pointer;
+    border-color: transparent;
+    border-width: 1px;
+    border-style: solid;
+  }
+
+  .arrow {
+    fill: var(--vt-c-white-soft);
+    width: 24px;
+    height: 24px;
+  }
+
+  .top-wrapper {
+    position: absolute;
+    width: calc(var(--sidebar-custom-width) + $margin);
+    background-color: var(--vt-c-black-soft);
+  }
+
+  &.expanded {
+    left: 0;
+    .arrow-ctn {
+      // border-width: 1px;
+      border-color: transparent transparent var(--vt-c-divider-dark-3) var(--vt-c-divider-dark-3);
+      // border-style: solid;
+    }
+    .arrow {
+      transform: rotate(-180deg);
+    }
+    .top-wrapper {
+      width: var(--sidebar-custom-width);
+    }
+  }
+
+  ul {
+    height: calc(100vh - $margin);
+    min-height: calc(100vh - $margin);
+    max-height: calc(100vh - $margin);
+    overflow-y: auto;
+    padding: 0 1.5em 1.5em 1.5em;
+    margin: $margin 0 0 0;
+    background-color: var(--vt-c-black-soft);
+
+    &::-webkit-scrollbar {
+      -webkit-appearance: none;
+      width: 8px;
+    }
+
+    &::-webkit-scrollbar-track {
+      background-color: var(--vt-c-black-mute);
+      border-radius: 8px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      border-radius: 8px;
+      background-color: var(--vt-c-divider-dark-3);
+    }
+  }
+}
+</style>
