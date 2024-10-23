94ab7f247e14d32d2cdbc997e6dc0f554e6cdc6c
ykogitos <ykogitos@gmail.com>
2024-10-23 10:15:04 +0200

    disabling components test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  component: {
-    devServer: {
-      framework: 'vue',
-      bundler: 'webpack'
-    },
-    specPattern: 'src/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'cypress/support/component.ts'
-  },
+  // component: {
+  //   devServer: {
+  //     framework: 'vue',
+  //     bundler: 'webpack'
+  //   },
+  //   specPattern: 'src/**/*.cy.{js,jsx,ts,tsx}',
+  //   supportFile: 'cypress/support/component.ts'
+  // },
   video: true,
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
