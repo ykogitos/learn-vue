078e98fe0b068e136a589afe6821c4c161000c03
ykogitos <y.kogitos@gmail.com>
2024-11-02 14:09:56 +0100

    extract methods

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -22,25 +22,30 @@ describe('WordleBoard', () => {
     })
   })
 
-  test('a victory message appears when the user makes a guess that matches the word of the day', async () => {
-   
+  async function playerTypesGuess(guess: string) {
+    await wrapper.find('input[type=text]').setValue(guess)
+  }
 
-    const guessInput = wrapper.find('input[type=text]')
-    await guessInput.setValue('TESTS')
-    await guessInput.trigger('keydown.enter')
+  async function playerPressesEnter() {
+    await wrapper.find('input[type=text]').trigger('keydown.enter')
+  }
+
+  async function playerTypesAndSubmitsGuess(guess: string) {
+    await playerTypesGuess(guess)
+    await playerPressesEnter()
+  }
+
+  test('a victory message appears when the user makes a guess that matches the word of the day', async () => {
+    await playerTypesAndSubmitsGuess(wordOfTheDay)
     expect(wrapper.text()).toContain(VICTORY_MESSAGE)
   })
 
   test('a defeat message appears when the user make an incorrect answer', async () => {
-
-    const guessInput = wrapper.find('input[type=text]')
-    await guessInput.setValue('WRONG')
-    await guessInput.trigger('keydown.enter')
+    await playerTypesAndSubmitsGuess('WRONG')
     expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
   })
 
   test('no end-of-game message appears if the user has not yet made a guess', async () => {
-
     expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
     expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE)
   })
