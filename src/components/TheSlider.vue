b0ca5d981ed27564cf1b0eb1a398d2595ce81ce5
ykogitos <y.kogitos@gmail.com>
2025-01-07 17:28:10 +0100

    adding content

--- src/components/TheSlider.vue
+++ src/components/TheSlider.vue
@@ -2,25 +2,62 @@
 import { ref } from 'vue'
 import ResponsivePicture from './images/ResponsivePicture.vue'
 
-const helloSlider = ref('Hello Slider')
+const helloSlider = ref('Slider ' + window.devicePixelRatio)
 
 const pics = [
-  'PIC_01',
-  'PIC_02',
-  'PIC_03',
-  'PIC_04',
-  'PIC_05',
-  'PIC_06',
-  'PIC_07',
-  'PIC_08',
-  'PIC_09',
-  'PIC_10',
-  'PIC_11'
+  {
+    picture: 'PIC_01',
+    name: 'Colosses de Memnon',
+    location: 'https://maps.app.goo.gl/wxpkGfFdtnSLKW9J8'
+  },
+  {
+    picture: 'PIC_02',
+    name: 'Karnak',
+    location: ''
+  },
+  {
+    picture: 'PIC_03',
+    name: 'Le Ramesséum',
+    location: 'https://maps.app.goo.gl/9iVJcGTegycJkZfF9'
+  },
+  {
+    picture: 'PIC_04',
+    name: 'Assouan',
+    location: 'https://maps.app.goo.gl/KQKY1dTsp2BsP9Mk7'
+  },
+  {
+    picture: 'PIC_05',
+    name: 'Temple de Karnak',
+    location: 'https://maps.app.goo.gl/c4PtJ9CB5Q5c2sMZ7'
+  },
+  {
+    picture: 'PIC_06',
+    name: 'Temple de Karnak'
+  },
+  {
+    picture: 'PIC_07',
+    name: 'Temple de Karnak'
+  },
+  {
+    picture: 'PIC_08',
+    name: 'Temple de Karnak'
+  },
+  {
+    picture: 'PIC_09',
+    name: 'Pyramides de Gizeh',
+    location: 'https://maps.app.goo.gl/cgeYv7WTt3ubSiBj9'
+  },
+  {
+    picture: 'PIC_10',
+    name: 'Temple de Karnak'
+  },
+  {
+    picture: 'PIC_11',
+    name: 'Île Éléphantine'
+  }
 ]
 
-// const pics = ['PIC_01', 'PIC_02']
 const widthRef = 250
-
 const width = ref(widthRef)
 const height = ref(Math.floor((widthRef * 3) / 4))
 const folder = ref('/images/slider')
@@ -30,9 +67,9 @@ const folder = ref('/images/slider')
   <h2>{{ helloSlider }}</h2>
   <div class="wrapper flex flex-row flex-fill">
     <div class="container" v-for="(pic, index) in pics" :key="`pic-${index}`">
-      <ResponsivePicture :folder="folder" :name="pic" :width="width" :height="height" />
-      <h2>{{ pic }}</h2>
-      <p>Lorem ipsum dolor sit amet</p>
+      <ResponsivePicture :folder="folder" :name="pic.picture" :width="width" :height="height" />
+      <h2>{{ pic.name }}</h2>
+      <p>{{ pic.picture }}</p>
     </div>
   </div>
 </template>
@@ -44,8 +81,14 @@ const folder = ref('/images/slider')
   overflow-y: auto;
   scroll-snap-type: x mandatory;
   scroll-behavior: smooth;
+  margin-left: -0.5rem;
+  padding-bottom: 1rem;
 }
 .container {
+  padding: 0.5rem;
+  margin-left: 0.5rem;
   scroll-snap-align: start;
+  border: 0.01rem solid var(--vt-c-divider-dark-3);
+  border-radius: 0.3rem;
 }
 </style>
--- src/components/images/ResponsivePicture.vue
+++ src/components/images/ResponsivePicture.vue
@@ -10,7 +10,7 @@ const props = defineProps<{
 <template>
   <img
     :src="`${props.folder}/${props.name}_DPI_1.jpg`"
-    :srcset="`${props.folder}/${props.name}_DPI_2.jpg 2x, ${props.folder}/${props.name}_DPI_3.jpg 9x`"
+    :srcset="`${props.folder}/${props.name}_DPI_2.jpg 2x, ${props.folder}/${props.name}_DPI_3.jpg 3x`"
     alt=""
     :width="props?.width ?? 250"
     :height="props?.height ?? 188"
