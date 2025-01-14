31b22ab64d8c851c8355dafbb69aa352c9b3ec71
ykogitos <y.kogitos@gmail.com>
2025-01-14 18:33:42 +0100

    add cypress slider

--- cypress/e2e/slider.cy.ts
+++ cypress/e2e/slider.cy.ts
@@ -1,4 +1,4 @@
-describe('Tic Tac Toe', () => {
+describe('SLIDER', () => {
   it('Check Slider', () => {
     cy.visit('/')
     cy.get('.arrow-ctn').click()
@@ -6,5 +6,10 @@ describe('Tic Tac Toe', () => {
     cy.get('nav ul li:nth-child(4)').click()
     cy.get('.aside').should('not.have.class', 'expanded')
     cy.url().should('include', 'the-slider')
+    cy.get('main > div').should('satisfy', ($el: HTMLAllCollection) => {
+      const classList = Array.from($el[0].classList)
+      return classList.includes('wrapper') && classList.includes('flex')
+    })
+    cy.get('main > div.wrapper').scrollTo(200, 0).invoke('scrollLeft').should('be.greaterThan', 100)
   })
 })
